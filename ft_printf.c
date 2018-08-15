bf7ca05fd0fd678c693edf836a03e48b322d5f13
ckoranec <jkertgat@e1z1r7p16.42.us.org>
2018-08-15 16:28:12 -0700

    g

--- ft_printf.c
+++ ft_printf.c
@@ -12,6 +12,23 @@
 #include "ft_printf.h"
 #include <limits.h>
 
+void	ft_longint(long long n)
+{
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else if (n < 0)
+	{
+		ft_putchar('-');
+		ft_longint(-n);
+	}
+	else if (n > 9)
+	{
+		ft_longint(n / 10);
+		ft_longint(n % 10);
+	}
+	else
+		ft_putchar(n + '0');
+}
 
 
 int putwstr(wchar_t *str)
