e80cb410c49d527b9c7ad45abaeb5a6ebb80989f
ckoranec <jkertgat@e1z1r7p16.42.us.org>
2018-08-15 16:15:51 -0700

    g

--- ft_printf.c
+++ ft_printf.c
@@ -12,6 +12,24 @@
 #include "ft_printf.h"
 #include <limits.h>
 
+int doer(t_varg *varg, t_lenmod *lenmod, va_list ap)
+{
+    if(varg->s == 1)
+    {
+        print_s(lenmod,ap);
+    }
+    if(varg->d == 1)
+    {
+        print_d_i(lenmod,ap);
+    }
+    if(varg->c == 1)
+    {
+        print_c(lenmod,ap);
+    }
+    return(1);
+}
+
+
 int putwstr(wchar_t *str)
 {
 int i;
