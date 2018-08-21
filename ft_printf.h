7af68739595f1a77c5b6a45cf348010568d115fe
ckoranec <jkertgat@e1z1r7p16.42.us.org>
2018-08-20 21:23:28 -0700

    .

--- ft_printf.h
+++ ft_printf.h
@@ -41,10 +41,12 @@ typedef struct s_lenmod
 	int zero;
 	int minus;
 	int plus;
+	int space;//added this ince there was nothing accounting for single space
 }				t_lenmod;
 void	ft_lontint2(long long n);
 int doer(t_varg *varg, t_lenmod *lenmod, va_list ap);
 int print_s(t_varg *varg,t_lenmod *lenmod, va_list ap);
 int print_d_i(t_varg *varg,t_lenmod *lenmod,va_list ap);
 int	print_c(t_varg *varg,t_lenmod *lenmod, va_list ap);
+int ft_printf(const char *str, ...);
 #endif
